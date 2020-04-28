48fc0ea4d15205fb26a6b5210aa2743d386f747d
Banu Prakash <m.banuprakashmib@gmail.com>
2020-04-28 14:10:01 +0530

    Update dob.js

--- dob.js
+++ dob.js
@@ -1,32 +1,32 @@
-var DOB = "December 26, 1996";
-var millisecondsBetweenDOBAnd1970 = Date.parse(DOB);
-var millisecondsBetweenNowAnd1970 = Date.now();
-var ageInMilliseconds = millisecondsBetweenNowAnd1970-millisecondsBetweenDOBAnd1970;
+function Results() {
+  var x = setInterval(() => {
+      var years, months, days, hours, seconds, milliseconds;
+      var DOB = new Date(document.getElementById('dob').value);
+      var millisecondsBetweenDOBAnd1970 = Date.parse(DOB);
+      var millisecondsBetweenNowAnd1970 = Date.now();
+      var ageInMilliseconds = millisecondsBetweenNowAnd1970 - millisecondsBetweenDOBAnd1970;
 
-  var milliseconds = ageInMilliseconds;
-  var second = 1000;
-  var minute = second*60;
-  var hour = minute*60;
-  var day = hour*24;
-  var month = day*30; 
+      var milliseconds = ageInMilliseconds;
+      var second = 1000;
+      var minute = second * 60;
+      var hour = minute * 60;
+      var day = hour * 24;
+      var month = day * 30;
 
-  var year = day*365;
+      var year = day * 365;
 
-var years = Math.round(milliseconds/year);
-var months = years*12;
-var days = years*365;
-var hours = Math.round(milliseconds/hour);
-var seconds = Math.round(milliseconds/second);
+      years = Math.round(milliseconds / year);
+      months = years * 12;
+      days = years * 365;
+      hours = Math.round(milliseconds / hour);
+      seconds = Math.round(milliseconds / second);
 
-function Results(){
-  var message = "Age in Years : "+years+
-     "</br>Age in Months : "+months+
-       "</br>Age in Days : "+days+
-      "</br>Age in Hours : "+hours+
-      "</br>Age in Seconds : "+seconds+
-      "</br>Age in Milliseconds : "+milliseconds;
-  document.getElementById('placeholder').innerHTML = message;
+      var message = "Age in Years : " + years +
+          "</br>Age in Months : " + months +
+          "</br>Age in Days : " + days +
+          "</br>Age in Hours : " + hours +
+          "</br>Age in Seconds : " + seconds +
+          "</br>Age in Milliseconds : " + milliseconds;
+      document.getElementById('placeholder').innerHTML = message;
+  }, 1000);
 }
-
-
-window.onload = Results;
